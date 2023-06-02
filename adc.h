bc866d979b50cbbdd9ef2b1b91098a789808e92e
ceng33620232 <ceng33620232@ceng33620232-virtualbox>
2023-06-02 22:47:34 +0300

    initial commit

---
+++ adc.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   adc.h
+ * Author: ceng33620232
+ *
+ * Created on June 2, 2023, 9:43 PM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+    
+void initADC();
+int readADChannel(); 
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC_H */
+
