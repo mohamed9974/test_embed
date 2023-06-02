8e4d60e0da817ee1519968ab8009b7a35e1193bf
ceng33620232 <ceng33620232@ceng33620232-virtualbox>
2023-06-02 23:13:59 +0300

    initial commit

--- adc.c
+++ adc.c
@@ -7,13 +7,13 @@ void initADC(){
     ADCON1bits.PCFG = 0b1110;
     
     ADCON0bits.CHS = 0 ;
-    ADCON0bits.ADON = 0 ;
+    
     ADCON2bits.ADFM = 1 ;
     ADCON2bits.ACQT = 2 ;
     ADCON2bits.ADCS = 6 ;
    
     PIE1bits.ADIE = 1;
-
+    ADCON0bits.ADON = 1 ;
 }
 
 int readADChannel(){
