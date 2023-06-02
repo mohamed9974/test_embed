40d5e5f5201d0b9c1a32c095649d074fba4af068
Mohamed Aly Amin <73635450+mohamed9974@users.noreply.github.com>
2023-06-02 23:10:10 +0300

    Update sevenSeg.c

--- sevenSeg.c
+++ sevenSeg.c
@@ -1,18 +1,19 @@
 #include <xc.h>
 #include "sevenSeg.h"
 
-unsigned char lookup_table[10]={
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111,
-    0b00111111
+unsigned char lookup_table[10] = {
+    0b00111111,  // 0
+    0b00000110,  // 1
+    0b01011011,  // 2
+    0b01001111,  // 3
+    0b01100110,  // 4
+    0b01101101,  // 5
+    0b01111101,  // 6
+    0b00000111,  // 7
+    0b01111111,  // 8
+    0b01101111   // 9
 };
+
 void initSevenSeg(){
     TRISAbits.RA3 = 0;
     TRISAbits.RA4 = 0;
