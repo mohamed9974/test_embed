63bf63b88b361814d42ade6d62d8c95bf12ca47a
Mohamed Aly Amin <73635450+mohamed9974@users.noreply.github.com>
2023-06-03 09:00:55 +0300

    Update adc.c

--- adc.c
+++ adc.c
@@ -1,26 +1,25 @@
 #include <xc.h>
 #include "adc.h"
 
-
 void initADC(){
     TRISAbits.RA0 = 1;
     ADCON1bits.PCFG = 0b1110;
-    ADCON1bits.VCFG = 0b00;
     
     ADCON0bits.CHS = 0 ;
-    
+    ADCON0bits.ADON = 1 ;
     ADCON2bits.ADFM = 1 ;
     ADCON2bits.ACQT = 2 ;
     ADCON2bits.ADCS = 6 ;
-   
-    IPR1bits.ADIP = 1;
-    PIE1bits.ADIE = 1;
-    ADCON0bits.ADON = 1 ;
 }
 
-int readADChannel(){
-    ADCON0bits.GO =1;
+void startADCConversion(){
+    ADCON0bits.GO = 1;
+}
+
+int isADCConversionComplete(){
+    return !ADCON0bits.GO;
+}
+
+int readADCResult(){
     return (ADRESH << 8) | ADRESL;
 }
-
-
